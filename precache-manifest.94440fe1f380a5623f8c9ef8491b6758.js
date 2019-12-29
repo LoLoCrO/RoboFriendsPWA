570fe9c4a74d1232d5f66d1978bad89aa95e0fc4
lolocro <bosniclovro@gmail.com>
2019-12-29 18:35:52 +0100

    Updates

---
+++ precache-manifest.94440fe1f380a5623f8c9ef8491b6758.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce0e1d28281b39a8b88c6c54a3b4fc24",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "689300ef00366bcac8ad",
+    "url": "/robofriends/static/css/main.0a2213a1.chunk.css"
+  },
+  {
+    "revision": "b65bbacc7fef517899b0",
+    "url": "/robofriends/static/js/2.a4f19978.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/robofriends/static/js/2.a4f19978.chunk.js.LICENSE"
+  },
+  {
+    "revision": "689300ef00366bcac8ad",
+    "url": "/robofriends/static/js/main.da45e95a.chunk.js"
+  },
+  {
+    "revision": "04454491b22da9ad3e7d",
+    "url": "/robofriends/static/js/runtime-main.98228a54.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
