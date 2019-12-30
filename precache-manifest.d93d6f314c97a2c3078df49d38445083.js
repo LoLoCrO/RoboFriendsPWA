515f33060de85249a182a13e7f7f8e0addbc18bf
lolocro <bosniclovro@gmail.com>
2019-12-30 10:37:36 +0100

    Updates

---
+++ precache-manifest.d93d6f314c97a2c3078df49d38445083.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e932ae7b84f54b10a51eeb593fcda5d1",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "fdad7afdabe3d9ba87ff",
+    "url": "/RoboFriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "43de8e630502be4405b8",
+    "url": "/RoboFriends/static/css/main.5666f206.chunk.css"
+  },
+  {
+    "revision": "fdad7afdabe3d9ba87ff",
+    "url": "/RoboFriends/static/js/2.15ef0a96.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/RoboFriends/static/js/2.15ef0a96.chunk.js.LICENSE"
+  },
+  {
+    "revision": "43de8e630502be4405b8",
+    "url": "/RoboFriends/static/js/main.29782e5a.chunk.js"
+  },
+  {
+    "revision": "9cffb152b5e5bcfa3e1e",
+    "url": "/RoboFriends/static/js/runtime-main.63030ebc.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/RoboFriends/precache-manifest.f85c980bbde29134a072505897efc563.js"
+  "/RoboFriends/precache-manifest.d93d6f314c97a2c3078df49d38445083.js"
 );
 
 self.addEventListener('message', (event) => {
